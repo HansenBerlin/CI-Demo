6aa11710dd2bf4a88d923c651e1fa305f6a141c4
LT-6FGD373\Hannes <h.roever@zertificon.com>
2022-03-24 13:33:22 +0100

    bugfix #12

--- PaymentApplication/Common/IHttpRequestController.cs
+++ PaymentApplication/Common/IHttpRequestController.cs
@@ -4,4 +4,5 @@ public interface IHttpRequestController
 {
     Task<HttpResponseMessage> GetAsync(string url);
     Task<HttpResponseMessage> PostAsync(string url, HttpContent content);
+    //test
 }
